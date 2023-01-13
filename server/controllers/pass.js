23f1a06249bc0650a11f798a19ad41b766142a57
Reshmika Nambiar <reshmikasnambiar@gmail.com>
2023-01-13 23:38:23 +0530

    sending emails for each request

--- server/controllers/pass.js
+++ server/controllers/pass.js
@@ -1,6 +1,8 @@
 import Current from "../models/current.js";
 import History from "../models/history.js";
 
+import nodemailer from "nodemailer";
+
 export const addPass = async (req, res) => {
   const {
     profile,
@@ -40,4 +42,47 @@ export const addPass = async (req, res) => {
   await currPass.save();
 
   console.log("Saved Pass");
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'eladio47@ethereal.email',
+        pass: 'yeXUHXy6PArZVWxECh'
+    }
+  });
+
+  console.log(profile.result.email);
+
+  let email = profile.result.email;
+
+  // console.log(profile.result)
+  console.log(email);
+
+  let messageOptions = {
+    from: 'eladio47@ethereal.email',
+    to: email,
+    subject: 'Your request has been submitted.',
+    text: 'Your concession request has been submitted.'
+  };
+
+  transporter.sendMail(messageOptions, function(error, info) {
+    if (error) {
+      throw error;
+    }
+  });
+
+  let messageOptions2 = {
+    from: 'eladio47@ethereal.email',
+    to: 'admin@vjti.com',
+    subject: 'Someone has put in a request',
+    text: 'Please go to Concession website and check'
+  };
+
+  transporter.sendMail(messageOptions2, function(error, info) {
+    if (error) {
+      throw error;
+    }
+  });
+
 };
