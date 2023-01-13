9a9f43c03e253f666614b8c57719279f6e863386
vaishnavi-13 <vaishnavi.bhadrashette@gmail.com>
2023-01-13 15:38:03 +0530

    user model, signup changed

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -26,7 +26,9 @@ export const signin = async (req, res) => {
 };
 
 export const signup = async (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
+
+  const { email, password, firstName, lastName,phoneNo, gender, dob,address, transportLine, regId, a_year, sem, program, department } = req.body;
+  //const { email, password, firstName, lastName } = req.body;
 
   try {
     const oldUser = await UserModal.findOne({ email });
@@ -35,7 +37,8 @@ export const signup = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+    const result = await UserModal.create({ email : email, password: hashedPassword, name: `${firstName} ${lastName}`,phoneNo: phoneNo,gender:  gender, dob: dob, address: address, transportLine: transportLine, regId : regId, a_year: a_year, sem: sem, program: program, department: department });
+    //const result = await UserModal.create({ email : email, password: hashedPassword, name: `${firstName} ${lastName}` });
 
     const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
 
@@ -46,3 +49,60 @@ export const signup = async (req, res) => {
     console.log(error);
   }
 };
+
+
+
+
+
+
+
+
+
+//@description     Edit new profile
+//@route           POST /api/users/
+//@access          Private
+
+// token + auth handling remaining in update profile
+
+export const updateProfile = async (req, res) => {
+  try
+  {
+    const user = await UserModal.findById(req.user._id);
+
+    if(user) 
+    {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      
+      //check pwd handling
+      if (req.body.password) {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        user.password = hashedPassword;
+      }
+
+      user.phoneNo = req.body.phoneNo || user.phoneNo;
+      user.gender = req.body.gender || user.gender;
+      user.dob = req.body.dob || user.dob;
+      user.address = req.body.address || user.address;
+      user.transportLine = req.body.transportLine || user.transportLine;
+      user.regId = req.body.regId || user.regId;
+      user.year = req.body.year || user.year;
+      user.sem = req.body.sem || user.sem;
+      user.program = req.body.program || user.program;
+      user.department = req.body.department || user.department;
+
+      const result = await user.save();
+      res.status(201).json({ result, token });
+    }
+    else{
+      res.status(400).json({message : "User not found"})
+    }  
+  }catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(error);
+  }
+
+
+};
+
+
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { signin, signup } from "../controllers/user.js";
+import { signin, signup, updateProfile } from "../controllers/user.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
+
+router.post("/updateProfile", updateProfile);
+
+
+router.get("/test", (req,res)=> res.send("Route testing done!") );
 
 export default router;
