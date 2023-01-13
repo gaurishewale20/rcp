e26e74408db3c1bb24cf7d32abd418ca8caf5b39
Prerna Jagesia <pkjagesia@gmail.com>
2023-01-13 21:05:01 +0530

    Added User Ref in Passes

--- server/models/current.js
+++ server/models/current.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 
-const currentSchema = mongoose.Schema({
-  StartDateCurr: { type: Date, required: true },
-  EndDateCurr: { type: Date, required: true },
-  StartStationCurr: { type: String, required: true },
-  EndStationCurr: { type: String, required: true },
-});
+const currentSchema = mongoose.Schema(
+  {
+    StartDateCurr: { type: Date, required: true },
+    EndDateCurr: { type: Date, required: true },
+    StartStationCurr: { type: String, required: true },
+    EndStationCurr: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status : {
+      type : String,
+      default : 'Pending'
+    }
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Current", currentSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -7,18 +7,28 @@ const userSchema = mongoose.Schema({
   id: { type: String },
 
   //extra profile details
-  phoneNo : { type: String, required:  false },
-  gender : {type : String , required:  false},
-  dob : {type : Date, required:  false},
-  address : {type: String, required:  false},
-  transportLine : { type : String, required:  false},
-  regId :{ type : String, required:  false},
-  year : {type: Number, required:  false},
-  sem : {type : Number, required:  false},
-  program : {type: String, required:  false},
-  department : {type : String, required:  false}
+  phoneNo: { type: String, required: false },
+  gender: { type: String, required: false },
+  dob: { type: Date, required: false },
+  address: { type: String, required: false },
+  transportLine: { type: String, required: false },
+  regId: { type: String, required: false },
+  year: { type: Number, required: false },
+  sem: { type: Number, required: false },
+  program: { type: String, required: false },
+  department: { type: String, required: false },
 
-
+  //passes
+  currentPass: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Current",
+  },
+  historyPass: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "History",
+    },
+  ],
 });
 
 export default mongoose.model("User", userSchema);
