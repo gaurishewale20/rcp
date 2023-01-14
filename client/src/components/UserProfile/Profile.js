8a85f3b03483aaa184d867e0e3abc629da49f496
meerawadher <wadher.meera@gmail.com>
2023-01-14 09:46:13 +0530

    Changes in Profile Styles

--- client/src/components/UserProfile/Profile.js
+++ client/src/components/UserProfile/Profile.js
@@ -12,6 +12,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Icon from "../Auth/icon";
 import { updateProfile } from "../../actions/updateProfile.js";
 import makeStyles from "../styles";
+import "./Profile.css";
 
 const Profile = () => {
 const formResult= JSON.parse(localStorage.getItem('profile'))
@@ -30,24 +31,26 @@ const formResult= JSON.parse(localStorage.getItem('profile'))
   const editProfile=()=>{
       history.push('/updateprofile');
   }
+
   return (
-    <Container component="main" maxWidth="xs">
-      <Button onClick={editProfile}>Edit Profile</Button>
-      <div className="container">
-       <h5>Name: {form.name}</h5>
-        <h5>Email: {form.email}</h5>
-        <h5>Phone No.: {form.phoneNo}</h5>
-        <h5>Gender: {form.gender}</h5>
-        <h5>Date Of Birth: {form.dob}</h5>
-        <h5>Transport Line {form.transportLine}</h5>
-        <h5>Address: {form.address}</h5>
-        <h5>Registration Id: {form.regId}</h5>
-        <h5>Academic Year: {form.a_year}</h5>
-        <h5>Semester: {form.sem}</h5>
-        <h5>Program: {form.program}</h5>
-        <h5>Department: {form.department}</h5>
-        <h5>VJTI ID Card Link: {form.vjti_id}</h5>
-        <h5>Aadhar Card Link: {form.aadhar_card}</h5>
+    <Container component="main">
+      {/* <Button onClick={editProfile} className="text-center">Edit Profile</Button> */}
+      <button type="submit" className="sub sub_new text-center" onClick={editProfile}>Edit Profile</button>
+      <div className="container user_info text-center">
+       <h4>Name: {form.name}</h4>
+        <h4>Email: {form.email}</h4>
+        <h4>Phone No.: {form.phoneNo}</h4>
+        <h4>Gender: {form.gender}</h4>
+        <h4>Date Of Birth: {form.dob}</h4>
+        <h4>Transport Line {form.transportLine}</h4>
+        <h4>Address: {form.address}</h4>
+        <h4>Registration Id: {form.regId}</h4>
+        <h4>Academic Year: {form.a_year}</h4>
+        <h4>Semester: {form.sem}</h4>
+        <h4>Program: {form.program}</h4>
+        <h4>Department: {form.department}</h4>
+        <h4>VJTI ID Card Link: {form.vjti_id}</h4>
+        <h4>Aadhar Card Link: {form.aadhar_card}</h4>
 
 
         {/* Write details sequencially */}
